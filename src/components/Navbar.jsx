ff17312c86aee57b9ded446947b7077b63b29404
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-08-20 19:24:36 +0530

    Added navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarLink,
+  MDBNavbarBrand,
+  MDBBadge,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import { useSelector, useDispatch } from "react-redux";
+import CartData from "../data";
+// import { displayItems } from "./redux/actions";
 
 const Navbar = () => {
+  const { totalCount } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <MDBNavbar expand="lg" dark bgColor="primary">
+        <MDBContainer>
+          <MDBNavbarBrand
+            style={{
+              alignItems: "center",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            Redux-Toolkit Shopping Cart
+          </MDBNavbarBrand>
 
-export default Navbar
+          <MDBNavbarLink>
+            <a className="mx-3" href="#!">
+              <MDBIcon fas icon="shopping-cart" size="lg" color="white" />
+              <MDBBadge
+                pill
+                color="danger"
+                notification
+                style={{ fontSize: "12px" }}
+              >
+                {totalCount}
+              </MDBBadge>
+            </a>
+          </MDBNavbarLink>
+        </MDBContainer>
+      </MDBNavbar>
+    </div>
+  );
+};
+
+export default Navbar;
